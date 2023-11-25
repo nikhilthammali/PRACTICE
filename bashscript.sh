c60623e387a9fe77bb4cc3c82366b25ce3ffbf4e
nikhilthammali <thammalanikhil68@gmail.com>
2023-11-25 07:32:04 +0000

    scritps

---
+++ bashscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "SYSTEM INFORMATION"
+
+echo "kernal version: $(uname -r)"
+
+echo "total memory: $(free -m)"
+
+echo "cpu info: $(uname -r)"
+
+
---
+++ bashscript2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter a number:"
+
+read num1
+
+echo "Enter another number:"
+
+read num2
+
+result=$((num1 + num2))
+
+echo "The result is: $result"
